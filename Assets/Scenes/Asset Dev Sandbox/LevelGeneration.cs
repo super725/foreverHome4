35ce46b6cd56e9113e7ee2ba8a7058b80e12c975
RayVanHorn <rvanhorn@csumb.edu>
2023-05-02 01:08:44 -0700

    Added borders and water to procgen
    Borders with smooth meshing
    still looking for a better water shader
    Need to loot and add invisible walls for player, but otherwise map is complete.

--- Assets/Scenes/Asset Dev Sandbox/LevelGeneration.cs
+++ Assets/Scenes/Asset Dev Sandbox/LevelGeneration.cs
@@ -7,14 +7,18 @@ public class LevelGeneration : MonoBehaviour
 {
 
     [Header("Level Properties")] 
-    public int mapWidthInTiles;
-    public int mapDepthInTiles;
-
+    public int rawMapWidth;
+    public int rawMapDepth;
+    public int borderThickness;
+    
+    
     [Header("Prefab List")] 
     public Prefab[] prefabs;
         
     [Header("Texture Properties")]
     public GameObject tilePrefab;
+    public GameObject flatPrefab;
+    public GameObject borderPrefab;
     public int tileResolution;
     public Texture2D texture1, texture2;
     public AnimationCurve blendCurve;
@@ -35,11 +39,16 @@ public class LevelGeneration : MonoBehaviour
     private Transform myTransform;
     private Vector3 myPosition;
     
-    [NonSerialized]
-    public int tileWidth, tileDepth;
-
+    [NonSerialized] public int tileWidth, tileDepth;
+    [NonSerialized] public int mapWidth, mapDepth;
     void Start()
     {
+        if (borderThickness < 2)
+        {
+            borderThickness = 2;
+        }
+        mapWidth = rawMapWidth + borderThickness * 2;
+        mapDepth = rawMapDepth + borderThickness * 2;
         // get the tile dimensions from the tile Prefab
         Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
         tileWidth = (int)tileSize.x;
@@ -52,17 +61,43 @@ public class LevelGeneration : MonoBehaviour
 
     IEnumerator GenerateMap() {
         // for each Tile, instantiate a Tile in the correct position
-        for (int xTileIndex = 0; xTileIndex < mapWidthInTiles; xTileIndex++) {
-            for (int zTileIndex = 0; zTileIndex < mapDepthInTiles; zTileIndex++) {
+        for (int xTileIndex = 0; xTileIndex < mapWidth; xTileIndex++) {
+            for (int zTileIndex = 0; zTileIndex < mapDepth; zTileIndex++) {
+                // EX. 2x2 playable, 4x4 border, 6x6flat tiles
+                // raw would be 2x2
+                // border thickness would be 2
+                // map width would be 6
+                
+                //bounds of flat would be noninclusive map-thickness = 6-2
+                
                 // calculate the tile position based on the X and Z indices
                 Vector3 tilePosition = new Vector3(myPosition.x + xTileIndex * tileWidth, myPosition.y, myPosition.z + zTileIndex * tileDepth);
-                Instantiate (tilePrefab, tilePosition, Quaternion.identity, myTransform);
+                
+                //  0-1 and 5-6
+                if((xTileIndex >= 0 && xTileIndex < borderThickness-1 || xTileIndex > mapWidth-borderThickness && xTileIndex <= mapWidth)
+                   || (zTileIndex >= 0 && zTileIndex < borderThickness-1 || zTileIndex > mapDepth-borderThickness && zTileIndex <= mapDepth)
+                   )
+                {
+                    // Flat
+                    Instantiate (flatPrefab, tilePosition, Quaternion.identity, myTransform);
+                }
+                // 2 and 4
+                else if((xTileIndex == borderThickness-1 || xTileIndex == mapWidth-borderThickness )
+                        || (zTileIndex == borderThickness-1 || zTileIndex == mapDepth-borderThickness))
+                {
+                    // Border
+                    Instantiate (borderPrefab, tilePosition, Quaternion.identity, myTransform);
+                }
+                else
+                {
+                    Instantiate (tilePrefab, tilePosition, Quaternion.identity, myTransform);
+                }
             }
         }
+        
         yield return 0; // Raycast collision doesnt work until after the mesh generation frame
         
-
-        Vector2 centerPos = new(mapWidthInTiles * tileWidth / 2f, mapDepthInTiles * tileDepth / 2f);
+        Vector2 centerPos = new(mapWidth * tileWidth / 2f, mapDepth * tileDepth / 2f);
         InstantiateObjectInMap(playerPrefab, centerPos.x, centerPos.y);
     }
     
